481b6e740d247bf5ff8379f01b6649f27cd2b699
Helene Sandvik <helenesandvik79@gmail.comgit config --global user.email helenesandvik79@gmail.comHelenes-MacBook-Pro-2:~ helene$ git config --global user.name Helene>
2018-05-29 21:18:18 +0200

    set in clock

--- www/main.js
+++ www/main.js
@@ -1,132 +1,101 @@
 // Klikker på startknappen og første del begynner å telle ned samtidig som startknappen 
 
-
-
 var button = document.querySelector(".button");
 button.addEventListener("click", start);
 
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var radius = canvas.height / 2;
+ctx.translate(radius, radius);
+radius = radius * 0.90
+drawNumbers(ctx, radius);
+
+//min for minutes, sec for seconds
+var timeType = 'min';
+
 function start(event){
-    
     var bilde = document.querySelector(".bilde");
     bilde.setAttribute('src', 'img/stillbilde_bg.gif');
+    var cs = document.getElementById('canvas');
     setVisible(document.getElementById('info_txt'), true);
     setVisible(document.getElementById('startBtn'), false);
+    setVisible(document.getElementById('triangle'),true);
+    
+    setVisible(cs, true);
     changeTxt('The pasta is undercooked');
-    setTimeout(changeTxt, 7000, 'Al dente');
-    setTimeout(changeTxt, 12000, 'Overcooked');
+    setTimeout(changeTxt, getWaitingTime(8, timeType), 'Al dente');
+    setTimeout(changeTxt, getWaitingTime(10, timeType), 'Overcooked');
+    addClass(cs, 'spinnable');
+    setTimeout(stop, getWaitingTime(12, timeType));   
+}
+
+function getWaitingTime(timeInSeconds, type) {
+    if (type == 'sec')
+        return timeInSeconds * 1000;
+    else if (type == 'min')
+        return (timeInSeconds * 1000) * 60;
+} 
+
+function stop() {
+    var cs = document.getElementById('canvas')
+    removeClass(cs, 'spinnable');
+    setVisible(document.getElementById('info_txt'), false);
+    setVisible(document.getElementById('startBtn'), true);
+    setVisible(document.getElementById('triangle'),false);
+    setVisible(cs, false);
+    
+    var bilde = document.querySelector(".bilde");
+    bilde.setAttribute('src', 'img/stillbilde_bg.png');
 }
 
 function setVisible(element,visible){
     if (visible){
-        element.className = element.className.replace('hidden','');
-        element.className += element.className.length > 0 ? ' visible' : 'visible'
+        removeClass(element, 'hidden');
+        addClass(element, 'visible');
     }
     else {
-        element.className = element.className.replace('visible','');
-        element.className += element.className.length > 0 ? ' hidden' : 'hidden'
+        removeClass(element, 'visible');
+        addClass(element, 'hidden');
+        
     }
 }
-      
+
+function addClass(element, cssClass){
+    element.className += element.className.length > 0 ? ' ' + cssClass : cssClass;
+}
+
+function removeClass(element, cssClass) {
+    element.className = element.className.replace(cssClass,'');
+}
+
 function changeTxt (message){
     document.getElementById('info_txt').innerHTML = message;
 }
 
-
-
-
-/*
-
-var duration={};
-
-duration.min=0;
-duration.sec=3;
-
-
-
-startButton.addEventListener("click", knappetrykkefunksjon);
-
-function knappetrykkefunksjon(event){
-    console.log(event);
+//Source: https://www.w3schools.com/graphics/canvas_clock_numbers.asp
+function drawNumbers(ctx, radius) {
+  var ang;
+  var num;
+  ctx.font = radius*0.15 + "px arial";
+  ctx.textBaseline="middle";
+  ctx.textAlign="center";
+  for(num = 1; num < 13; num++){
+    ang = num * Math.PI / 6;
     
-    
-//skjule knappen etter at den er trykke på
-    
-    startButton.classList.add("hidden");
-    unitTextObject.classList.add("hidden");
-    
-    body.classList.add("active");
-   
-// start intervall timer
-    
-    var intervalObj=setInterval(handleInterval, 1000);
-    
-    function handleInterval(){
-        
-        if(duration.min== 0 && duration.sec==0){
-            
-            clearInterval(intervalObj);
-            playAlarm();
-            
-        }else{
-            
-            countDown();
-            showDuration();
-        }
-    }
-    
-    
-    }
-
-function playAlarm(){
-    timeTextObject.textContent = "Grattis";
-    setTimeout (resetClock, 3000);
-    
+    if(num < 8)
+        ctx.fillStyle = 'red';
+    else if(num > 9)
+        ctx.fillStyle ='black';
+    else 
+        ctx.fillStyle ='green';
+      
+    ctx.rotate(ang);
+    ctx.translate(0, -radius*0.85);
+    ctx.rotate(-ang);
+    ctx.fillText(num.toString(), 0, 0);
+    ctx.rotate(ang);
+    ctx.translate(0, radius*0.85);
+    ctx.rotate(-ang);
+  }
 }
-
-function resetClock(){
-    
-    duration.min =25;
-    timeTextObject.textContent=duration.min;
-    
-    startButton.classList.remove("hidden");
-    unitTextObject.classList.remove("hidden");
-    body.classList.remove("active");
-    
-}
-
-
-//Nedtellingsfunksjon
-
-function countDown(){
-    
-    duration.sec = duration.sec -1;
-    
-    if(duration.sec < 0){
-        duration.min=duration.min -1;
-        duration.sec=59;
-    }
-    console.log(duration);
-    
-}
-    
-    function showDuration(){
-        
-        var min=duration.min;
-        var sec=duration.sec;
-        
-        if(min<10){
-            min="0" + min;
-        }
-        
-        if(sec < 10){
-            sec="0" + sec;
-        }
-        timeTextObject.textContent=min + ":" + sec;
-    }
-
-
-
-*/
-
-
 
